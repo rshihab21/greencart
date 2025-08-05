05f2c575ea07790a9c5af265248f6dc3ae810bb8
rshihab21 <shihabcse21@gmail.com>
2025-08-05 15:33:39 +0600

    design implement of main banner section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import MainBanner from "../components/MainBanner";
+
+const Home = () => {
+  return (
+    <div className="mt-10">
+      <MainBanner></MainBanner>
+    </div>
+  );
+};
+
+export default Home;
