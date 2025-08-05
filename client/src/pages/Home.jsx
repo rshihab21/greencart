26a1cd075021c09f17c0ee049da626cc21cde00d
rshihab21 <shihabcse21@gmail.com>
2025-08-05 16:06:00 +0600

    design implement categories section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Categories from "../components/Categories";
 import MainBanner from "../components/MainBanner";
 
 const Home = () => {
   return (
     <div className="mt-10">
       <MainBanner></MainBanner>
+      <Categories></Categories>
     </div>
   );
 };
